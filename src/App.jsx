b692eb4959975d4c7057a45738f85ae168faadab
Bhuvan <bhuvanms31@gmail.com>
2023-08-26 20:43:16 +0530

    added URL reference to API

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ function App() {
   return (
     <>    
       <h1 className="text-3xl font-bold underline">Food Details API</h1>  
+      <br/>
+      <p>Built with <a className='text-green-400' href='https://www.themealdb.com/'>TheMealDb API </a> by <a className='text-green-400' href='https://github.com/bhuvan-ms'>Bhuvan MS</a></p>
+      <br/>
       <hr className="my-12 h-0.5 border-t-0 bg-neutral-100 opacity-100 dark:opacity-50" />
       <SearchBar/>     
     </>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className=" text-black p-4 fixed bottom-0 w-full">
+      <div className=" text-center">
+        <p>Built with <a className='text-green-400' href='https://www.themealdb.com/'>TheMealDb API </a> by <a className='text-green-400' href='https://github.com/bhuvan-ms'>Bhuvan MS</a></p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
